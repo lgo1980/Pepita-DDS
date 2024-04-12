5b01795cd2a33f642c43443e2263dc883382173d
Leonardo Olmedo <lgo80@yahoo.com.ar>
2024-04-12 16:10:03 -0300

    first commit

---
+++ src/main/java/ar/edu/utn/frba/dds/Golondrina.java
@@ -0,0 +1,27 @@
+package ar.edu.utn.frba.dds;
+
+public class Golondrina {
+	
+	private int energia;
+
+	public Golondrina(int energia) {
+		this.energia = energia;
+	}
+
+	public int getEnergia() {
+		return energia;
+	}
+
+	public void setEnergia(int energia) {
+		this.energia = energia;
+	}
+	
+	public void comer(int calorias) {
+		this.energia += calorias * 3; 
+	}
+	
+	public void volar() {
+		this.energia -= 10; 
+	}
+
+}
