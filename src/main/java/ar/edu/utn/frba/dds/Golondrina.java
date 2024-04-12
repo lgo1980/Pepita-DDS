5acfe136dc9e50227526f905164eb545fe9c6b02
Leonardo Olmedo <lgo80@yahoo.com.ar>
2024-04-12 16:37:09 -0300

    1º arreglo

--- src/main/java/ar/edu/utn/frba/dds/Golondrina.java
+++ src/main/java/ar/edu/utn/frba/dds/Golondrina.java
@@ -1,27 +1,27 @@
 package ar.edu.utn.frba.dds;
 
 public class Golondrina {
-	
-	private int energia;
 
-	public Golondrina(int energia) {
-		this.energia = energia;
-	}
+  private int energia;
 
-	public int getEnergia() {
-		return energia;
-	}
+  public Golondrina(int energia) {
+    this.energia = energia;
+  }
 
-	public void setEnergia(int energia) {
-		this.energia = energia;
-	}
-	
-	public void comer(int calorias) {
-		this.energia += calorias * 3; 
-	}
-	
-	public void volar() {
-		this.energia -= 10; 
-	}
+  public int getEnergia() {
+    return energia;
+  }
+
+  public void setEnergia(int energia) {
+    this.energia = energia;
+  }
+
+  public void comer(int calorias) {
+    this.energia += calorias * 3;
+  }
+
+  public void volar() {
+    this.energia -= 10;
+  }
 
 }
